454398161db9e65351dad7cc10a2987a9942172e
Ikram Chouichi <ikram@netronian.com>
2024-02-04 21:30:37 -0500

    Putting it All Together: State and Events

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,48 @@
-import React from "react";
+// App.js
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const filterTasksByCategory = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const addTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const removeTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  // Filter tasks based on the selected category
+  const filteredTasks = selectedCategory === "All"
+    ? tasks
+    : tasks.filter((task) => task.category === selectedCategory);
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onCategoryChange={filterTasksByCategory}
+      />
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={addTask}
+      />
+      <TaskList
+        tasks={filteredTasks}
+        onRemove={removeTask}
+      />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,13 @@
+// TaskList.js
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, onRemove }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onRemove={onRemove} />
+      ))}
     </div>
   );
 }
